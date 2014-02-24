154759b63a3eb06234e1fa67286ead38bbb27066
vanessamontanha <vanessamontanha@gmail.com>
2014-02-24 13:25:44 +0000

    This is the first commit of a cakePHP application

---
+++ app/Controller/PostsController.php
@@ -0,0 +1,13 @@
+<?Php
+
+class PostsController extends AppController {
+    public $helpers = array('Html', 'Form');
+
+    public function index() {
+        $this->set('posts', $this->Post->find('all'));
+    }
+}
+
+
+
+?>
