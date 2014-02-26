f2543fb0ed441515a9da872816f87e2948c7da29
vanessamontanha <vanessamontanha@gmail.com>
2014-02-26 20:51:44 +0000

    validation configured

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -23,7 +23,6 @@ class PostsController extends AppController {
 
 //this function will allow us to add to the posts database
 public function add() {
-
 		//checking if this a HTTP post request
         if ($this->request->is('post')) {
 		//initialising the Post Model 
@@ -39,17 +38,22 @@ public function add() {
             $this->Session->setFlash(__('Unable to add your post.'));
         }
     }
-public function edit($id = null) {
+
+	
+	//this function will allow us to edit an existing post
+	public function edit($id = null) {
     if (!$id) {
         throw new NotFoundException(__('Invalid post'));
     }
-
+	//
     $post = $this->Post->findById($id);
     if (!$post) {
         throw new NotFoundException(__('Invalid post'));
     }
-
+	
+	
     if ($this->request->is(array('post', 'put'))) {
+	
         $this->Post->id = $id;
         if ($this->Post->save($this->request->data)) {
             $this->Session->setFlash(__('Your post has been updated.'));
@@ -62,17 +66,23 @@ public function edit($id = null) {
         $this->request->data = $post;
     }
 }
+
 public function delete($id) {
+	//if this is an HTTP get not allow
     if ($this->request->is('get')) {
         throw new MethodNotAllowedException();
     }
 
+	//we are calling the delete Post method
     if ($this->Post->delete($id)) {
+	
         $this->Session->setFlash(
+		
             __('The post with id: %s has been deleted.', h($id))
         );
         return $this->redirect(array('action' => 'index'));
     }
 }
-}
+	}
+
 ?>
