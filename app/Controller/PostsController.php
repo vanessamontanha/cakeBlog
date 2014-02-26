3850896ddf5ba3599e971269e3a6898b0d6b6422
vanessamontanha <vanessamontanha@gmail.com>
2014-02-26 16:34:57 +0000

    completed code

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -1,13 +1,78 @@
-<?Php
-
+<?php
 class PostsController extends AppController {
-    public $helpers = array('Html', 'Form');
+
+	public $helpers = array('Html', 'Form', 'Session');
+    public $components = array('Session');
 
     public function index() {
-        $this->set('posts', $this->Post->find('all'));
+		//finding all the records in the Post table and handling the response to the index of ctp
+         $this->set('posts', $this->Post->find('all'));
+    }
+
+    public function view($id = null) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        $post = $this->Post->findById($id);
+        if (!$post) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+        $this->set('post', $post);
+ }
+
+//this function will allow us to add to the posts database
+public function add() {
+
+		//checking if this a HTTP post request
+        if ($this->request->is('post')) {
+		//initialising the Post Model 
+            $this->Post->create();
+			//handling the request object data to be saved by the Post Model
+            if ($this->Post->save($this->request->data)) {
+			//flashing a success message
+			$this->Session->setFlash(__('Your post has been saved.'));
+			//redirecting to the Posts indec action
+                return $this->redirect(array('action' => 'index'));
+			}
+			//flash a message saying we weren't able to do it
+            $this->Session->setFlash(__('Unable to add your post.'));
+        }
+    }
+public function edit($id = null) {
+    if (!$id) {
+        throw new NotFoundException(__('Invalid post'));
+    }
+
+    $post = $this->Post->findById($id);
+    if (!$post) {
+        throw new NotFoundException(__('Invalid post'));
+    }
+
+    if ($this->request->is(array('post', 'put'))) {
+        $this->Post->id = $id;
+        if ($this->Post->save($this->request->data)) {
+            $this->Session->setFlash(__('Your post has been updated.'));
+            return $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash(__('Unable to update your post.'));
+    }
+
+    if (!$this->request->data) {
+        $this->request->data = $post;
     }
 }
+public function delete($id) {
+    if ($this->request->is('get')) {
+        throw new MethodNotAllowedException();
+    }
 
-
-
+    if ($this->Post->delete($id)) {
+        $this->Session->setFlash(
+            __('The post with id: %s has been deleted.', h($id))
+        );
+        return $this->redirect(array('action' => 'index'));
+    }
+}
+}
 ?>
--- app/Model/Posts/Post.php
+++ app/Model/Posts/Post.php
@@ -1,2 +1,14 @@
 class Post extends AppModel {
+    public $validate = array(
+        'title' => array(
+            'rule' => 'notEmpty'
+			'required' => true
+			 'message' => 'Password must be at least 8 characters long'
+        ),
+        'body' => array(
+            'rule' => 'notEmpty'
+			'required' => true
+			 'message' => 'Password must be at least 8 characters long'
+        )
+    );
 }
