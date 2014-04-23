8088cf995b63c493771a2114ead923220be96f56
vanessamontanha <vanessamontanha@gmail.com>
2014-04-23 05:01:05 +0100

    New core.php

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@ Configure::write('Routing.prefixes', array('admin'));
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93bIUJJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'MYhG93b0qyJfIxfs2gu3VoUbWwvniR2G0Fga9mi');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496723224968348454645');
+	Configure::write('Security.cipherSeed', '71159309657453542496749683645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
