5e9c3c812562404f7a6916916c995b389005cb0b
vanessamontanha <vanessamontanha@gmail.com>
2014-04-23 04:53:17 +0100

    New Core.php with fixed details

--- app/Config/core.php
+++ app/Config/core.php
@@ -31,7 +31,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 2);
+	Configure::write('debug', 0);
 
 /**
  * Configure the Error handler used to handle errors for your application. By default
@@ -227,7 +227,7 @@ Configure::write('Routing.prefixes', array('admin'));
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '768593096574535424967232249683645');
+	Configure::write('Security.cipherSeed', '76859309657453542496723224968348454645');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
